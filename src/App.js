b7b28685e62cbf1fd34858186e965cae35f890a8
Dario Purpi <dariopurpi@gmail.com>
2023-01-23 17:30:04 +0100

    esercitazione 23-01-23

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar/navbarLinks.jsx
@@ -0,0 +1,32 @@
+import { FaBeer } from "react-icons/fa";
+
+const navBarLinks = [
+  {
+    id: 1,
+    label: "Home",
+    route: "home",
+    icon: <FaBeer />,
+  },
+  {
+    id: 2,
+    label: "Categories",
+    route: "categories",
+  },
+  {
+    id: 3,
+    label: "Prodocuts",
+    route: "products",
+  },
+  {
+    id: 4,
+    label: "Orders",
+    route: "orders",
+  },
+  {
+    id: 5,
+    label: "User",
+    route: "users",
+  },
+];
+
+export default navBarLinks;
